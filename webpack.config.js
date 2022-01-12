0fb504470f37f26ff663021e4785287c57a78dce
twgd <twgd.huang@gmail.com>
2022-01-13 01:26:38 +0800

    feat: fonts demo

--- webpack.config.js
+++ webpack.config.js
@@ -50,6 +50,13 @@ module.exports = (env) => {
             filename: 'images/[hash][ext][query]',
           },
         },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'fonts/[name][ext]',
+          },
+        },
       ],
     },
     plugins: [
